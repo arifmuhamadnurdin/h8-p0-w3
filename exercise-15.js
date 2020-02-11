1a0ef40521dbbc07d4dbd93866fe2e2e7b879ece
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2020-02-11 23:31:22 +0700

    update

--- exercise-15.js
+++ exercise-15.js
@@ -1,20 +1,21 @@
+
 function groupAnimals(animals) {
   // you can only write your code here!
-  var tampung = [ [] ]
-  var a = 0
-  animals.sort()
-  tampung[0].push(animals[0])
-    for(var i = 1 ; i < animals.length ; i++){
-        if(animals[i][0] === tampung[a][0][0]){ 
-            tampung[a].push(animals[i])
-      }else{
-      a++
-      tampung[a] = [animals[i]] 
-          }
+  var hasil = [];
+  var str = 'abcdefghijklmnopqrstuvwxyz'
+  for(var i=0; i<str.length; i++){
+    var arr = []
+    for(var j=0; j<animals.length; j++){
+      if(str[i]===animals[j][0]){
+        arr.push(animals[j])
+      }
+    }
+    if(arr.length>0){
+      hasil.push(arr)
+    }
   }
-    return tampung
+  return hasil;
 }
-
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
